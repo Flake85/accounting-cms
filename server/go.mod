caf341926cd33f4274ece08250c118a03b7b10bd
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-07-21 16:21:17 -0500

    WIP - connecting to db

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,9 @@
 module server
 
 go 1.17
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func main() {
+  db, err := gorm.Open("postgres", "host=localhost port=54321 user=cms dbname=cms password=cms sslmode=disable")
+  if err != nil {
+	fmt.Printf("db connection error: %v", err)
+  }
+  defer db.Close()
+}
