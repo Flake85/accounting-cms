d615a1997b60f20c3f4dce91efca80a8f2ec4ec7
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-08-03 19:08:33 -0500

    fixed and added error handling

--- server/handlers/clienthandler.go
+++ server/handlers/clienthandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"server/model"
@@ -15,7 +16,8 @@ func GetClients(w http.ResponseWriter, r *http.Request) {
 	clients, err := repository.GetAllClients()
 	if err != nil {
 		log.Println("error occurred getting clients")
-		w.WriteHeader(400)
+		w.WriteHeader(500)
+		fmt.Fprintln(w, "error occurred retrieving clients")
 		return
 	}
 	json.NewEncoder(w).Encode(&clients)
@@ -27,12 +29,14 @@ func GetClient(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("%v, is not a valid uuid.", clientIdParam)
 		w.WriteHeader(400)
+		fmt.Fprintln(w, "invalid uuid")
 		return
 	}
 	client, err := repository.FindClientByID(clientId)
 	if err != nil {
 		log.Printf("client: %v, not found", clientIdParam)
-		w.WriteHeader(400)
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "client not found")
 		return
 	}
 	json.NewEncoder(w).Encode(&client)
@@ -40,11 +44,17 @@ func GetClient(w http.ResponseWriter, r *http.Request) {
 
 func CreateClient(w http.ResponseWriter, r *http.Request) {
 	var client model.Client
-	json.NewDecoder(r.Body).Decode(&client)
+	if err := json.NewDecoder(r.Body).Decode(&client); err != nil {
+		log.Print("client decode malfunction")
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "an error occurred creating client")
+		return
+	}
 	clientId, err := repository.CreateClient(&client) 
 	if err != nil {
 		log.Printf("client: %v, not created", clientId)
-		w.WriteHeader(400)
+		w.WriteHeader(500)
+		fmt.Fprintln(w, "an error occurred creating client")
 		return
 	}
 	client.ID = clientId
@@ -57,23 +67,31 @@ func UpdateClient(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("%v, is not a valid uuid.", clientIdParam)
 		w.WriteHeader(400)
+		fmt.Fprintln(w, "an error occurred updating client")
 		return
 	}
 	client, err := repository.FindClientByID(clientId)
 	if err != nil {
 		log.Printf("client not found with uuid: %v", clientId)
-		w.WriteHeader(400)
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "client not found")
 		return
 	}
 	req := model.Client{}
-	json.NewDecoder(r.Body).Decode(&req)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Print("client decode malfunction")
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "an error occurred updating client")
+		return
+	}
 	client.Name = req.Name
 	client.Email = req.Email
 	client.Address = req.Address
 	
 	if err := repository.UpdateClient(&client); err != nil {
 		log.Printf("error occurred updating client id: %v", clientId)
-		w.WriteHeader(400)
+		w.WriteHeader(500)
+		fmt.Fprintln(w, "an error occurred updating client")
 		return
 	}
 	json.NewEncoder(w).Encode(&client)
