21434f1860173ea05ffa5f314142efb3846ff786
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-08-03 20:07:57 -0500

    fixed fmt syntax error

--- server/repository/clientRepository.go
+++ server/repository/clientRepository.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 func FindClientByID(clientId uuid.UUID) (model.Client, error) {
 	client := model.Client{}
 	if DB.Where("id = ?", clientId).Find(&client).Error != nil {
-		return client, fmt.Errorf("Cannot find client by id: %v", DB.Error)
+		return client, fmt.Errorf("Cannot find client by id: %w", DB.Error)
 	}
 	return client, nil
 }
@@ -21,21 +21,21 @@ func FindClientByID(clientId uuid.UUID) (model.Client, error) {
 func GetAllClients() ([]model.Client, error) {
 	clients := make([]model.Client, 0)
 	if DB.Find(&clients).Error != nil {
-		return clients, fmt.Errorf("could not get clients from db: %v", DB.Error)
+		return clients, fmt.Errorf("could not get clients from db: %w", DB.Error)
 	}
 	return clients, nil
 }
 
 func CreateClient(client *model.Client) (uuid.UUID, error) {
 	if DB.Create(client).Error != nil {
-		return client.ID, fmt.Errorf("cannot create client: %v", DB.Error)
+		return client.ID, fmt.Errorf("cannot create client: %w", DB.Error)
 	}
 	return client.ID, nil
 }
 
 func UpdateClient(client *model.Client) error {
 	if DB.Save(client).Error != nil {
-		return fmt.Errorf("cannot create client: %v", DB.Error)
+		return fmt.Errorf("cannot create client: %w", DB.Error)
 	}
 	return nil
 }
