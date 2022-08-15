dca8962383bde4e9bfc566138d62be89e7d93149
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-08-15 13:33:22 -0500

    refactored api response

--- server/response/apiresponse.go
+++ server/response/apiresponse.go
@@ -5,14 +5,9 @@ import (
 	"net/http"
 )
 
-type ApiResponse struct {
-	Code int			 `json:"code"`
-	Body ApiResponseBody `json:"body"`
-}
-
 type ApiResponseBody struct {
 	Data    interface{} `json:"data,omitempty"`
-	Error   interface{} `json:"error,omitempty"`
+	Error   ApiError    `json:"error,omitempty"`
 }
 
 type ApiError struct {
@@ -21,38 +16,23 @@ type ApiError struct {
 }
 
 func NewOkResponse(data interface{}, w http.ResponseWriter) {
-	res := ApiResponse{
-		Code: 200,
-		Body: ApiResponseBody{
-			Data: data,
-		},
-	}
-	json.NewEncoder(w).Encode(res)
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(ApiResponseBody{Data: data})
 }
 
 func NewErrorResponse(code int, message string, w http.ResponseWriter) {
-	res := ApiResponse{
-		Code: code,
-		Body: ApiResponseBody{
-			Error: ApiError{
-				Message: message,
-			},
-		},
-	}
-	w.WriteHeader(res.Code)
-	json.NewEncoder(w).Encode(res)
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(ApiResponseBody{
+		Error: ApiError{Message: message},
+	})
 }
 
 func NewErrorResponseWithData(code int, message string, data interface{}, w http.ResponseWriter) {
-	res := ApiResponse{
-		Code: code,
-		Body: ApiResponseBody{
-			Error: ApiError{
-				Message: message,
-				Data: data,
-			},
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(ApiResponseBody{
+		Error: ApiError{
+			Message: message,
+			Data: data,
 		},
-	}
-	w.WriteHeader(res.Code)
-	json.NewEncoder(w).Encode(res)
+	})
 }
