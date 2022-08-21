4004995abedf8456178ce06eac12640857f1869f
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-08-21 17:25:11 -0500

    FE: changed home to dashboard; removed custom 500

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,45 @@
+import Card from "react-bootstrap/Card"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 export default function Home() {
     return (
-      <div>
-          <h3><i class="bi-house"></i>Home</h3>
-          <h3><i class="bi-people"></i>Clients</h3>
-          <h3><i class=""></i>Invoices</h3>
-          <h3><i class=""></i>Labor</h3>
-          <h3><i class=""></i>Sales</h3>
-          <h3><i class=""></i>Expenses</h3>
-      </div>
-    )
+      <Card className="text-center">
+        <Card.Header>Dashboard</Card.Header>
+        <Card.Body>
+            <Row>
+              <Col>
+                <Card.Link href="/client">
+                  <h3><i class="bi-people"></i></h3>
+                  <h4>Clients</h4>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link href="#">
+                  <h3><i class="bi-receipt"></i></h3>
+                  <h4>Invoices</h4>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link href="#">
+                  <h3><i class="bi-hammer"></i></h3>
+                  <h4>Labor</h4>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link href="#">
+                  <h3><i class="bi-receipt-cutoff"></i></h3>
+                  <h4>Sales</h4>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link href="#">
+                  <h3><i class="bi-cash-coin"></i></h3>
+                  <h4>Expenses</h4>
+                </Card.Link>
+              </Col>
+            </Row>
+        </Card.Body>
+      </Card>
+    );
 }
