db5bfbf50933552acad6c1f3fe189b459b36eebe
Flechas Bentley <Flechas.Bentley@gmail.com>
2022-08-21 13:20:24 -0500

    bootstrap icons and update home page

--- client/pages/client/[cid].js
+++ client/pages/client/[cid].js
@@ -1,6 +1,7 @@
 import Alert from 'react-bootstrap/Alert'
 
 export default function Client({ client }) {
+    // change to ternary 
     { if(client.data) {return (
         <div>
             <p>name: {client.data.name}</p>
@@ -23,6 +24,7 @@ export async function getServerSideProps(context) {
     const res = await fetch(`${process.env.BASEURL}/client/${id}`)
     const client = await res.json()
 
+    // change this up. its overkill
     return res.status === 404 ? { redirect: { destination: "/404" } }
          : res.status === 500 ? { redirect: { destination: "/500" } }
          : { props: {client} }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
       <div>
-          <h1>Home</h1>  
+          <h3><i class="bi-house"></i>Home</h3>
+          <h3><i class="bi-people"></i>Clients</h3>
+          <h3><i class=""></i>Invoices</h3>
+          <h3><i class=""></i>Labor</h3>
+          <h3><i class=""></i>Sales</h3>
+          <h3><i class=""></i>Expenses</h3>
       </div>
     )
 }
